912fc87f59fc555043f76ce028e008a5c259e487
Corentin Chappelet <corentin.chappelet@icloud.com>
2026-04-27 15:33:26 +0200

    config obesoindair

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const AGENT_CONFIG = {
   agentName: "Alex",
   primaryColor: "#2C1810",
   accentColor: "#8B6F47",
-
   hours: {
     lundi: "Midi uniquement",
     mardi: "Midi uniquement",
@@ -13,25 +12,20 @@ const AGENT_CONFIG = {
     samedi: "Midi et soir",
     dimanche: "Midi uniquement",
   },
-
   menu: {
     entrees: ["Spécialités savoyardes", "Cuisine traditionnelle française", "Produits frais de saison"],
     plats: ["Plats savoyards", "Cuisine du terroir", "Carte de printemps du 24/04 au 28/06 2026"],
     desserts: ["Desserts maison", "Suggestions du chef"],
     menus: ["Consultez notre carte sur le site pour les tarifs"],
   },
-
   address: "Cité médiévale de Conflans, Albertville, Savoie",
   phone: "04 79 37 71 29",
   email: "obesoindair@outlook.fr",
   parking: "Grande place piétonne de Conflans",
   access: "Face au musée et à l'hôtel La Citadelle, cité médiévale de Conflans",
-
   reservationUrl: "https://www.obesoindair.com/PBCPPlayer.asp?ID=1992695",
   reservationPhone: "04 79 37 71 29",
   maxPartySize: 20,
-
   dietary: "Cuisine traditionnelle française et savoyarde. Contactez-nous pour toute allergie.",
-
   defaultLanguage: "fr",
 };
