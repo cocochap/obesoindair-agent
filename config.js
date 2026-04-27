54e408ec795ea64383b401d28bd8840faa0e39ca
Corentin Chappelet <corentin.chappelet@icloud.com>
2026-04-27 15:52:11 +0200

    menu complet obesoindair

--- config.js
+++ config.js
@@ -3,29 +3,59 @@ const AGENT_CONFIG = {
   agentName: "Alex",
   primaryColor: "#2C1810",
   accentColor: "#8B6F47",
+
   hours: {
-    lundi: "Midi uniquement",
-    mardi: "Midi uniquement",
-    mercredi: "Fermé",
-    jeudi: "Fermé",
-    vendredi: "Midi et soir",
-    samedi: "Midi et soir",
-    dimanche: "Midi uniquement",
+    lundi: "12h00 - 13h30 (midi uniquement, fermé le soir)",
+    mardi: "12h00 - 13h30 (midi uniquement, fermé le soir)",
+    mercredi: "Fermé toute la journée",
+    jeudi: "Fermé toute la journée",
+    vendredi: "12h00 - 13h30 / 19h00 - 20h45",
+    samedi: "12h00 - 13h30 / 19h00 - 20h45",
+    dimanche: "12h00 - 13h30 (midi uniquement, fermé le soir)",
   },
+
   menu: {
-    entrees: ["Spécialités savoyardes", "Cuisine traditionnelle française", "Produits frais de saison"],
-    plats: ["Plats savoyards", "Cuisine du terroir", "Carte de printemps du 24/04 au 28/06 2026"],
-    desserts: ["Desserts maison", "Suggestions du chef"],
-    menus: ["Consultez notre carte sur le site pour les tarifs"],
+    entrees: [
+      "Crème froide de carottes et lentilles corail (13€)",
+      "Œuf parfait, crème de petit pois, sérac et pesto de basilic (13€)",
+      "Gravelax de truite rose mariné au fenouil, duo de fenouil et citron confit (13€ + supplément 4€)",
+      "Panna cotta d'asperges blanches, poitrine fumée et crumble au beaufort (13€)",
+    ],
+    plats: [
+      "Filet de turbot, risotto de riz noir, poireau rôti au miel et moutarde, féta et noix (20€)",
+      "Ris de veau, pomme de terre mitraille et champignons de Paris (20€ + supplément 10€)",
+      "Filet de daurade grillé, mousseline de choux rouge au wasabi et déclinaison de choux (20€)",
+      "Filet de canette sauce au miel et épices, polente crémeuse aux abricots et raisins secs (20€)",
+      "Entrecôte frites légumes (28€)",
+      "Fondue Savoyarde avec charcuterie - minimum 2 personnes (23€)",
+      "Tartiflette et son mesclun (20€)",
+    ],
+    desserts: [
+      "Assiette de fromages (9€)",
+      "Pavlova fraise basilic (9€)",
+      "Le citron menthe façon mojito - sablé breton, gel citron menthe, crème citron et menthe (9€)",
+      "Chou Paris-Brest au grué de cacao (9€)",
+    ],
+    menus: [
+      "Menu Savoyard 30€ : Velouté de légumes + Tartiflette ou Fondue Savoyarde + Dessert",
+      "Menu du jour 25€ (midi uniquement, hors dimanche et jours fériés) : Entrée + Plat + Dessert",
+      "Formule midi entrée/plat 21€ ou plat/dessert 19€",
+      "Plat du jour 15€ (midi uniquement, hors dimanche et jours fériés)",
+      "Menu enfant 13€ : Jambon ou Velouté + Viande/Poisson frites + Dessert",
+    ],
   },
-  address: "Cité médiévale de Conflans, Albertville, Savoie",
+
+  address: "17 Place de Conflans, 73200 Albertville",
   phone: "04 79 37 71 29",
   email: "obesoindair@outlook.fr",
   parking: "Grande place piétonne de Conflans",
-  access: "Face au musée et à l'hôtel La Citadelle, cité médiévale de Conflans",
+  access: "Face au musée et à l'hôtel La Citadelle, cité médiévale de Conflans, Albertville",
+
   reservationUrl: "https://www.obesoindair.com/PBCPPlayer.asp?ID=1992695",
   reservationPhone: "04 79 37 71 29",
   maxPartySize: 20,
-  dietary: "Cuisine traditionnelle française et savoyarde. Contactez-nous pour toute allergie.",
+
+  dietary: "Cuisine traditionnelle française et savoyarde. Pour toute allergie ou régime spécifique, contactez-nous au 04 79 37 71 29 ou par mail à obesoindair@outlook.fr. La Fondue Savoyarde est disponible pour minimum 2 personnes.",
+
   defaultLanguage: "fr",
 };
